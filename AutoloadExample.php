13cc49d55d2ea41390e5789e572a08e7ae8bc9e0
diogenespetry <diogenes.petry@gmail.com>
2016-04-08 19:34:18 -0300

    correção

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -5,7 +5,7 @@ namespace dirs\dataehora;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class AutoloadExample extends \yii\helpers
 {
     public function run()
     {
