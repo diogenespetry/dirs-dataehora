1ae4307e5735d2b23d53cc5c1ee566b79d667324
diogenespetry <diogenes.petry@gmail.com>
2016-04-08 13:06:09 -0300

    Added files via upload

---
+++ git.cmd
@@ -0,0 +1,6 @@
+echo "# dirs-dataehora" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/diogenespetry/dirs-dataehora.git
+git push -u origin master
